42a96c8d116ec20de3ed7d39463ac126192c03ea
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-09 12:54:58 -0400

    estableciendo la estructura del contrato

--- chat.sol
+++ chat.sol
@@ -1,5 +1,21 @@
 pragma solidity >=0.8.9;
 
 contract Chat {
-    mapping(address => string) ownerMessages;
+    // event when a user write a new message
+    event newMessage(string message, address owner);
+
+    // Message object
+    struct Message {
+        uint id;
+        string message;
+        address owner;
+    }
+
+    // this contract has many messages
+    Message[] public messages;
+
+    // given the id, return the user
+    mapping(uint => address) public messageToOwner;
+    // given the address, return how messages has written
+    mapping(address => uint) ownerMessageCount;
 }
