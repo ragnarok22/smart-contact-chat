4aa455f98e4fde8941cbb1fad10d849df5d7fdc6
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-10 13:19:02 -0400

    mostrando un nuevo mensaje cuando llega

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,28 +1,41 @@
-import { useEffect, useState } from "react"
-import { ChatList } from "./ChatList";
+import {useEffect, useState} from "react"
+import {ChatList} from "./ChatList";
 
-export const Chat = ({ web3, accounts, contract}) => {
+export const Chat = ({web3, accounts, contract}) => {
   const [loading, setLoading] = useState(true);
   const [messages, setMessages] = useState([]);
-
+  
   useEffect(() => {
     (async () => {
+      // get all the messages
       const response = await contract.methods.getAllMessages().call();
-      console.log(response);
       setMessages(response);
     })()
+    
+    // suscribe to new message
+    contract.events.newMessage().on("data", (e) => {
+      let message = e.returnValues;
+      setMessages((prevState) => [
+        ...prevState,
+        {
+          id: message.id,
+          owner: message.owner,
+          message: message.message,
+        }
+      ])
+    }).on("error", console.error);
     setLoading(false);
-
+    
   }, []);
   
   const onSubmit = async (e) => {
     e.preventDefault();
     const text = e.target.firstChild.value
     console.log('enviando ' + text)
-    await contract.methods.addMessage(text).send({ from: accounts[0] });
+    await contract.methods.addMessage(text).send({from: accounts[0]});
     console.log('mensaje enviado')
   }
-
+  
   if (loading) {
     return (
       <div>
@@ -30,13 +43,13 @@ export const Chat = ({ web3, accounts, contract}) => {
       </div>
     )
   }
-
+  
   return (
     <div>
       <h1>Chat room</h1>
-      <ChatList messages={messages} />
+      <ChatList messages={messages}/>
       <form onSubmit={onSubmit}>
-        <input type="text" />
+        <input type="text"/>
       </form>
     </div>
   )
